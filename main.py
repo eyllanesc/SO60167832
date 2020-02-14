a0a1f25145feb0fa95156b40c9e806fb5bc17ffa
eyllanesc <e.yllanescucho@gmail.com>
2020-02-14 07:11:28 -0500

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import select
 import subprocess
 import threading
 
@@ -6,7 +8,6 @@ from PyQt5 import QtCore
 
 class Worker(QtCore.QObject):
     outSignal = QtCore.pyqtSignal(str)
-    errSignal = QtCore.pyqtSignal(str)
 
     def run_command(self, cmd, **kwargs):
         threading.Thread(
@@ -14,18 +15,14 @@ class Worker(QtCore.QObject):
         ).start()
 
     def _execute_command(self, cmd, **kwargs):
-        with subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs
-        ) as proc:
-            out, err = proc.communicate()
-            if out:
-                self.outSignal.emit(out.decode())
-            if err:
-                self.errSignal.emit(err.decode())
+        proc = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, **kwargs
+        )
+        for line in proc.stdout:
+            self.outSignal.emit(line.decode())
 
 
 if __name__ == "__main__":
-    import os
     import sys
 
     app = QtCore.QCoreApplication(sys.argv)
@@ -35,12 +32,12 @@ if __name__ == "__main__":
         print(data)
 
     w.outSignal.connect(print)
-    w.errSignal.connect(print)
 
     w.run_command("test.bat", cwd="./", shell=True)
-    w.run_command("cd", cwd="./", shell=True)
-    w.run_command("whoami", cwd="./", shell=True)
-    w.run_command("dir", cwd="./", shell=True)
+    # w.run_command("cd", cwd="./", shell=True)
+    # w.run_command("whoami", cwd="./", shell=True)
+    # w.run_command("dir", cwd="./", shell=True)
+    w.run_command(["ping", "8.8.8.8"])
 
     QtCore.QTimer.singleShot(60 * 1000, QtCore.QCoreApplication.quit)
 
